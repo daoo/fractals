09c54051840f07c0efc9e74940a6a271a2799cd8
daoo <daoo314@gmail.com>
2013-05-05 11:18:28 +0200

    Refactor

---
+++ src/Fractals/Args.hs
@@ -0,0 +1,35 @@
+module Fractals.Args where
+
+import Control.Applicative
+import Control.Monad.State
+import Fractals.Complex
+import Fractals.Definitions
+import Fractals.Fractal
+
+pop :: State [String] String
+pop = fmap head get >>= \s -> modify tail >> return s
+
+popPoint :: Read a => State [String] (a, a)
+popPoint = do
+  x <- pop
+  y <- pop
+  return (read x, read y)
+
+popComp :: State [String] Comp
+popComp = uncurry (:+) `fmap` popPoint
+
+parseArgs :: [String] -> Fractal
+parseArgs args = (`evalState` args) $ do
+  str <- pop
+
+  fractal <- case str of
+    "mandelbrot"  -> (mandelbrot . read) <$> pop
+    "burningship" -> return burningShip
+    "julia"       -> julia <$> popComp
+    _             -> undefined
+
+  iters   <- read <$> pop
+  screen  <- popPoint
+  topleft <- popPoint
+  plane   <- popPoint
+  return $ Fractal fractal iters 4 topleft plane screen
