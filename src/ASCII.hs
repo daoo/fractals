c6f6ada68f9a80d571f84baa9a6a2268d0446744
daoo <daoo314@gmail.com>
2013-05-06 14:28:16 +0200

    Remove the complex build time config
    Have to come up with something better.

--- src/ASCII.hs
+++ src/ASCII.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 module Main where
 
 import Fractals.Area
@@ -9,26 +8,11 @@ import Fractals.Definitions
 import Fractals.Utility
 import System.Environment
 
-{-# INLINE parse #-}
-parse :: [String] -> (Fractal, [String])
-#ifdef MANDELBROT2
-parse = parseFractal1 (return mandelbrot2')
-#elif MANDELBROT
-parse = parseFractal1 parseMandelbrot
-#elif JULIA
-parse = parseFractal1 parseJulia
-#elif BURNINGSHIP
-parse = parseFractal1 (return burningShip)
-#else
-parse = parseFractal
-#endif
-
 main :: IO ()
 main = do
-  (f, _) <- parse `fmap` getArgs
+  (f, _) <- parseFractal `fmap` getArgs
   putStr $ showFractal (fractalDefinition f) (fractalIter f) (fractalMaxAbs f) (fractalArea f)
 
-{-# INLINE showFractal #-}
 showFractal :: Definition -> Int -> R -> Area -> String
 showFractal fractal iter maxabs (Area topleft _ delta screen) =
   unlines $ grid screen topleft delta (ascii iter .: func)
