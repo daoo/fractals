532b2d81e77e0297c939ad5b59218e217ed2aa08
daoo <daoo314@gmail.com>
2013-05-08 17:01:18 +0200

    Add benchmarking with criterion

---
+++ src/Benchmark.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Control.Monad
+import Criterion.Main
+import Fractals.Area
+import Fractals.Definitions
+import Fractals.Output
+
+test :: Definition -> IO ()
+test def = void $ array def 100 4 area
+  where
+    screen@(w, h) = (1000, 1000)
+    aspect = realToFrac w / realToFrac h
+
+    area = fromRectangle
+      screen
+      (-4.3 / 2.0, 4.3 / aspect / 2.0)
+      (4.3, 4.3 / aspect)
+
+main :: IO ()
+main = defaultMain
+  [ bench "mandelbrot 2" $ test (mandelbrot 2)
+  , bench "mandelbrot2'" $ test mandelbrot2'
+  ]
