843f84614440c3c6880ba8bf7f950f81ae57df37
daoo <daoo314@gmail.com>
2013-05-13 15:58:19 +0200

    Add ImageArray Greyscale instance

--- src/Fractals/Image.hs
+++ src/Fractals/Image.hs
@@ -19,6 +19,25 @@ class (Monad m, MArray a e m, Color c, Ix i) => ImageArray c a i e m where
   write :: Image c a i e -> Int -> c -> m ()
   create :: MArray a e m => (Int -> Int -> c) -> Definition -> Int -> R -> Area -> m (Image c a i e)
 
+instance ImageArray Greyscale IOUArray (Int, Int) Word8 IO where
+  {-# INLINE new #-}
+  new (w, h) = Image <$> newArray_ ((0,0), (h-1,w-1))
+
+  {-# INLINE write #-}
+  write (Image arr) n c = do
+    unsafeWrite arr n c
+
+  {-# INLINE create #-}
+  create !color !fractal !iter !maxabs !area = do
+    i <- new (areaScreen area)
+    fillArray
+      (areaScreen area)
+      (areaTopLeft area)
+      (areaDelta area)
+      (\n x y -> write i n $ color iter $ fractal (x:+y) maxabs iter)
+      (+1)
+    return i
+
 instance ImageArray RGBA IOUArray (Int, Int, Int) Word8 IO where
   {-# INLINE new #-}
   new (w, h) = Image <$> newArray_ ((0,0,0), (h-1,w-1,3))
