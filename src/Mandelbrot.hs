47cb02816e229fd329b3d758527f3bd2e1fe11d4
daoo <daoo314@gmail.com>
2013-05-15 14:15:09 +0200

    Clean up and add benchmarks

--- src/Mandelbrot.hs
+++ src/Mandelbrot.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Codec.Image.DevIL
+import Data.Array.IO (IOUArray)
 import Data.Array.Unsafe
 import Fractals.Area
 import Fractals.Coloring
@@ -12,7 +13,7 @@ import Fractals.Utility
 main :: IO ()
 main = do
   ilInit
-  Image arr <- create (toRgba `xy` greyscale) mandelbrot2 200 4 area :: IO RgbaImage
+  Image arr <- create (toRgba `xy` greyscale) mandelbrot2 200 4 area :: IO (Image RGBA IOUArray (Int, Int, Int) Word8)
   unsafeFreeze arr >>= writeImage "dist/mandelbrot.png"
   where
     area = aspectCentered (1920, 1080) 4.3 (-2.0:+0)
