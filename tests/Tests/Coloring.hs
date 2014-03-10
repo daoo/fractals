da628ee32dfa33fb81d321e8a80c7775e87008bb
daoo <daoo314@gmail.com>
2014-03-10 09:59:24 +0100

    Delete obsolete test suite

--- tests/Tests/Coloring.hs
+++
@@ -1,8 +0,0 @@
-module Tests.Coloring where
-
-import Fractals.Coloring
-import Test.SmallCheck
-import Test.SmallCheck.Series
-
-propRgbaRgbInv :: RGB -> Bool
-propRgbaRgbInv rgb = toRgb (toRgba rgb) == rgb
